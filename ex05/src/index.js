5bccac3f7090174f169c807897e0532c4de1384b
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-20 23:54:04 +0100

    ex05 gotov

---
+++ ex05/src/index.js
@@ -0,0 +1,20 @@
+myPetsArray = [
+    {
+        animalType:"Dog",
+        name:"Pujdo"
+    },
+    {
+        animalType:"Cat",
+        name:"maca"
+    },
+    {
+        animalType:"Bird",
+        name:"Tweety"
+    }
+]
+function myPetsFunction(pets){
+return pets[1].name;
+}
+console.log(myPetsFunction(myPetsArray));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
